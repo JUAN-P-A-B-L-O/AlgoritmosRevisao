6abaaf027f6d421d7414f72c8f843f2d107eb874
Juan Eugenio <juan.eugenio@elogroup.com.br>
2024-06-13 23:15:00 -0300

    Encontre o maior valor no array - ex

---
+++ RecursividadeExercicios/4_2_AchaValorMaisAlto.js
@@ -0,0 +1,11 @@
+const arrNumeros = [12, 144,-4, -2, 334, 30, 5, 11]
+const maiorValor = achaMaiorValor(arrNumeros)
+
+console.log(maiorValor)
+
+function achaMaiorValor(arrNumeros, maiorValor = arrNumeros[0]) {
+    const valorAtual = arrNumeros.shift();
+    const valorRetornado = valorAtual > maiorValor ? valorAtual : maiorValor
+
+    return arrNumeros.length === 0 ? valorRetornado : achaMaiorValor(arrNumeros, valorRetornado);
+}
